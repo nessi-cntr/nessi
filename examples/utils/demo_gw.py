7a60b2d5be962235964ecce2c407f656f3a0f37e
michaelschueler <michael.schueler@unifr.ch>
2019-10-05 21:20:32 -0700

    added field

--- examples/utils/demo_gw.py
+++ examples/utils/demo_gw.py
@@ -2,7 +2,6 @@ import sys
 import os
 import glob
 import numpy as np
-#import matplotlib.pyplot as plt
 from ReadCNTR import write_input_file
 #----------------------------------------------------------------------
 def merge_two_dicts(x, y):
@@ -10,25 +9,35 @@ def merge_two_dicts(x, y):
     z.update(y)    # modifies z with y's keys and values & returns None
     return z
 #----------------------------------------------------------------------
-def GenSysParams(Nk,HoppingT,HubbardU,V,MuChem,Beta,FermBos=-1):
+def GenField(E0,omega,Np,Nt,dt,file_field):
+    ts = np.linspace(0.0,Nt*dt,Nt+1)
+    t0 = 2.0*np.pi/(omega*Np)
+    Ef = E0 * np.exp(-4.6*(ts-t0)**2/t0**2)*np.sin(omega*(ts-t0))
+    Ef = np.insert(Ef, 0, 0.0)
+    np.savetxt(file_field,Ef,fmt='%.10f')
+#----------------------------------------------------------------------
+def GenSysParams(Nk,HoppingT,HubbardU,V,MuChem,Beta):
     sysparams = {'Nk': Nk,
                  'HoppingT': HoppingT,
                  'HubbardU': HubbardU,
                  'V': V,
                  'MuChem': MuChem,
-                 'beta': Beta,
-                 'FermBos': FermBos
+                 'beta': Beta
         }
     return sysparams
 #----------------------------------------------------------------------
-def GenSolverParams(Nt,Ntau,dt,SaveGreen=False,MatsMaxIter=100,
+def GenSolverParams(Nt,Ntau,dt,SaveGreen=False,SaveMomentum=False,MatsMaxIter=100,
                         MatsMaxErr= 1.0e-8,
                         BootstrapMaxIter= 20,BootstrapMaxErr=1.0e-8,
-                        TimeMaxErr=1.0e-8,CorrectorSteps=10,SolverOrder=5):
+                        TimeMaxErr=1.0e-8,CorrectorSteps=10):
 
     savegf = 0
     if SaveGreen:
         savegf = 1
+
+    savegk = 0
+    if SaveMomentum:
+        savegk = 1
     
     solverparams = {'Nt': Nt,
                     'Ntau': Ntau,
@@ -39,42 +48,40 @@ def GenSolverParams(Nt,Ntau,dt,SaveGreen=False,MatsMaxIter=100,
                     'BootstrapMaxErr': BootstrapMaxErr,
                     'TimeMaxErr': TimeMaxErr,
                     'CorrectorSteps': CorrectorSteps,
-                    'SolverOrder': SolverOrder,
-                    'SaveGreen': savegf
+                    'SaveGreen': savegf,
+                    'SaveMomentum': savegk
                         }
     return solverparams
 #----------------------------------------------------------------------
-def GenInputFile(input_file,sysparams,solverparams):
+def GenInputFile(input_file,sysparams,solverparams,file_field):
     inp = sysparams
     inp = merge_two_dicts(inp, solverparams)
+    inp = merge_two_dicts(inp, {'Epulse': '--'+file_field})
     write_input_file(inp, input_file)
 #----------------------------------------------------------------------
-def Run(sysparams,solverparams,output_file,
+def Run(sysparams,solverparams,file_field,output_file,mpicmd,
                         input_file='',log_file='',runpath=''):
-    ns = sysparams['Nk']
+    Nk = sysparams['Nk']
     U = sysparams['HubbardU']
+    V = sysparams['V']
     Nt = solverparams['Nt']
     Ntau = solverparams['Ntau']
     dt = solverparams['dt']
     if len(input_file) == 0:
-        flin = './inp/hubbardchain_N{}_U{}_Nt{}_Ntau{}_dt{}.inp'.format(ns,U,Nt,Ntau,dt)
+        flin = './inp/gw_Nk{}_U{}_V{}_Nt{}_Ntau{}_dt{}.inp'.format(Nk,U,V,Nt,Ntau,dt)
     else:
         flin = input_file
-    GenInputFile(flin,sysparams,solverparams)
+    GenInputFile(flin,sysparams,solverparams,file_field)
     prog = runpath + '/exe/gw.x'
 
     log_flag = ''
     if len(log_file) > 0:
         log_flag = ' > ' + log_file
         
-    os.system("mpirun " + prog + ' ' + flin + ' ' + output_file + log_flag)
+    os.system(mpicmd + ' ' + prog + ' ' + flin + ' ' + output_file + log_flag)
 #----------------------------------------------------------------------     
 
 if __name__ == '__main__':
-
-    os.chdir("build")
-    os.system("../osx_cmake.sh")
-    os.chdir("../")
 
     Nk=10
     thop=1.0
@@ -85,29 +92,20 @@ if __name__ == '__main__':
     sysparams = GenSysParams(Nk,thop,U,V,mu,beta)
 
     Nt = 200
-    Ntau = 700
+    Ntau = 200
     dt = 0.01
     solverparams = GenSolverParams(Nt,Ntau,dt,MatsMaxErr= 1.0e-10,BootstrapMaxErr=1.0e-10)
 
-    #fig,(ax1,ax2) = plt.subplots(2,1,sharex=True)
+    # field 
+    E0 = 0.0
+    Np = 1
+    omega = 1.0
+    file_field = 'inp/Epulse_E{}_Np{}_w{}.txt'.format(E0,Np,omega)
+    GenField(E0,omega,Np,Nt,dt,file_field)
 
     color_palette = ['blue', 'orange', 'green', 'red']
-
-    #ax2.set_xlabel(r'$t$')
-    #ax2.set_ylabel('energy')
-    #ax1.set_ylabel(r'$n_1(t)$')
     
     output_file = 'out/gw_'
-    Run(sysparams,solverparams,output_file,runpath='./')
-
-    data = np.loadtxt(output_file + '_occupation.dat')
-    t = data[:,0]        
-    #ax1.plot(t,data[:,1],label=approx,c=color_palette[i])
-
-    t,Ekin,Epot,Etot = np.loadtxt(output_file + '_energy.dat',unpack=True)
-    #ax2.plot(t,Ekin,c=color_palette[i])
-    #ax2.plot(t,Etot,c=color_palette[i],linestyle='--')
-
-    #ax1.legend(loc='upper right',frameon=False)
-    #plt.show()
+    mpicmd = 'mpirun -n 2'
+    Run(sysparams,solverparams,file_field,output_file,mpicmd,runpath='./')
 
