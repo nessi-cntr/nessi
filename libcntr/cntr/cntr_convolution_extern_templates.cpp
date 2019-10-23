62f421c8a6be93563f5c4ee6d2ff3e8b0a75accc
Michael Schueler <michael.schueler@unifr.ch>
2019-10-22 17:03:03 -0700

    bugfix

--- libcntr/cntr/cntr_convolution_extern_templates.cpp
+++ libcntr/cntr/cntr_convolution_extern_templates.cpp
@@ -85,7 +85,7 @@ template void
 convolution_timestep_omp<double>(int omp_num_threads, int tstp, herm_matrix<double> &C, herm_matrix<double> &A,
                         herm_matrix<double> &Acc, herm_matrix<double> &B,
                         herm_matrix<double> &Bcc,
-                        double beta, double h int SolveOrder);
+                        double beta, double h, int SolveOrder);
 template void
 convolution_omp<double>(int omp_num_threads, herm_matrix<double> &C, herm_matrix<double> &A,
                         herm_matrix<double> &Acc, herm_matrix<double> &B,
