4be4ca011e8e0ddc3acfc302e9ee95a3d407bde9
Michael Schueler <michael.schueler@unifr.ch>
2019-10-22 17:18:37 -0700

    corrected interfaces

--- libcntr/cntr/cntr_convolution_extern_templates.cpp
+++ libcntr/cntr/cntr_convolution_extern_templates.cpp
@@ -60,37 +60,78 @@ template
                                 integration::Integrator<double> &I, double beta, double h);
   
 #if CNTR_USE_OMP==1
-template void convolution_matsubara_omp<double, herm_matrix<double> >(int nomp, herm_matrix<double> &C,herm_matrix<double> &A,herm_matrix<double> &B,integration::Integrator<double> &I, double beta);
-template void convolution_timestep_omp<double>(int omp_num_threads,int tstp,herm_matrix<double> &C,herm_matrix<double> &A, herm_matrix<double> &B,integration::Integrator<double> &I, double beta,double h);
-template void convolution_omp<double>(int omp_num_threads,herm_matrix<double> &C,herm_matrix<double> &A,herm_matrix<double> &Acc, herm_matrix<double> &B,herm_matrix<double> &Bcc, integration::Integrator<double> &I, double beta,double h);
-template void
-convolution_timestep_omp<double>(int omp_num_threads, int tstp,  herm_matrix<double> &C, herm_matrix<double> &A,
-                        herm_matrix<double> &Acc, herm_matrix<double> &B,
-                        herm_matrix<double> &Bcc, integration::Integrator<double> &I,
-                        double beta, double h);
-template void
-convolution_omp<double>(int omp_num_threads, herm_matrix<double> &C, herm_matrix<double> &A,
-                        herm_matrix<double> &Acc, herm_matrix<double> &B,
-                        herm_matrix<double> &Bcc, integration::Integrator<double> &I,
-                        double beta, double h);
 
 
-template void 
-convolution_timestep_omp<double>(int omp_num_threads,int tstp,herm_matrix<double> &C,
-	herm_matrix<double> &A, herm_matrix<double> &B, double beta,double h, int SolveOrder);
-template void 
-convolution_omp<double>(int omp_num_threads,herm_matrix<double> &C,herm_matrix<double> &A,
-	herm_matrix<double> &B,double beta,double h, int SolveOrder);
-template void
-convolution_timestep_omp<double>(int omp_num_threads, int tstp, herm_matrix<double> &C, herm_matrix<double> &A,
-                        herm_matrix<double> &Acc, herm_matrix<double> &B,
-                        herm_matrix<double> &Bcc,
-                        double beta, double h, int SolveOrder);
-template void
-convolution_omp<double>(int omp_num_threads, herm_matrix<double> &C, herm_matrix<double> &A,
-                        herm_matrix<double> &Acc, herm_matrix<double> &B,
-                        herm_matrix<double> &Bcc,
-                        double beta, double h, int SolveOrder);
+template
+void convolution_timestep_omp<double>(int omp_num_threads, int tstp, herm_matrix<double> &C,
+                              herm_matrix<double> &A, herm_matrix<double> &Acc, function<double> &ft,
+                              herm_matrix<double> &B, herm_matrix<double> &Bcc,
+                              integration::Integrator<double> &I, double beta, double h);
+
+template 
+void convolution_timestep_omp<double>(int omp_num_threads, int tstp, herm_matrix<double> &C,
+                              herm_matrix<double> &A, function<double> &ft, herm_matrix<double> &B,
+                              integration::Integrator<double> &I, double beta, double h);
+
+template 
+void convolution_matsubara_omp<double>(int omp_num_threads, herm_matrix<double> &C, herm_matrix<double> &A,
+                               herm_matrix<double> &B, integration::Integrator<double> &I, double beta);
+
+template 
+void convolution_matsubara_omp<double>(int omp_num_threads, herm_matrix<double> &C, herm_matrix<double> &A,
+                               function<double> &ft, herm_matrix<double> &B,
+                               integration::Integrator<double> &I, double beta);
+
+template 
+void convolution_omp<double>(int omp_num_threads, herm_matrix<double> &C, herm_matrix<double> &A,
+                     herm_matrix<double> &Acc, function<double> &ft, herm_matrix<double> &B,
+                     herm_matrix<double> &Bcc, integration::Integrator<double> &I, double beta, double h);
+
+template 
+void convolution_timestep_omp<double>(int omp_num_threads, int tstp, herm_matrix<double> &C,
+                              herm_matrix<double> &A, herm_matrix<double> &Acc, herm_matrix<double> &B,
+                              herm_matrix<double> &Bcc, integration::Integrator<double> &I, double beta,
+                              double h);
+
+template 
+void convolution_timestep_omp<double>(int omp_num_threads, int tstp, herm_matrix<double> &C,
+                              herm_matrix<double> &A, herm_matrix<double> &B,
+                              integration::Integrator<double> &I, double beta, double h);
+
+template
+void convolution_omp<double>(int omp_num_threads, herm_matrix<double> &C, herm_matrix<double> &A,
+                     herm_matrix<double> &Acc, herm_matrix<double> &B, herm_matrix<double> &Bcc,
+                     integration::Integrator<double> &I, double beta, double h);
+
+// simplified new interfaces 
+template
+void convolution_timestep_omp<double>(int omp_num_threads, int tstp, herm_matrix<double> &C,
+                              herm_matrix<double> &A, herm_matrix<double> &Acc, function<double> &ft,
+                              herm_matrix<double> &B, herm_matrix<double> &Bcc,
+                              double beta, double h, int SolveOrder);
+template
+void convolution_timestep_omp<double>(int omp_num_threads, int tstp, herm_matrix<double> &C,
+                              herm_matrix<double> &A, function<double> &ft, herm_matrix<double> &B,
+                              double beta, double h, int SolveOrder);
+
+template
+void convolution_omp<double>(int omp_num_threads, herm_matrix<double> &C, herm_matrix<double> &A,
+                     herm_matrix<double> &Acc, function<double> &ft, herm_matrix<double> &B,
+                     herm_matrix<double> &Bcc, double beta, double h, int SolveOrder);
+template
+void convolution_timestep_omp<double>(int omp_num_threads, int tstp, herm_matrix<double> &C,
+                              herm_matrix<double> &A, herm_matrix<double> &Acc, herm_matrix<double> &B,
+                              herm_matrix<double> &Bcc, double beta,
+                              double h, int SolveOrder);
+template
+void convolution_timestep_omp<double>(int omp_num_threads, int tstp, herm_matrix<double> &C,
+                              herm_matrix<double> &A, herm_matrix<double> &B,
+                              double beta, double h, int SolveOrder);
+template
+void convolution_omp<double>(int omp_num_threads, herm_matrix<double> &C, herm_matrix<double> &A,
+                     herm_matrix<double> &Acc, herm_matrix<double> &B, herm_matrix<double> &Bcc,
+                     double beta, double h, int SolveOrder);
+
 
 #endif
 
